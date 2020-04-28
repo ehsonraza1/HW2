951b425403a9197a56fc11262de985d2b2fbfa28
Ehson Raza <ehson.raza1@outlook.com>
2020-04-27 20:54:18 -0500

    Second Commit

--- script.js
+++ script.js
@@ -17,8 +17,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCaseArr        = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr        = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr           = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacterArr = ['!','@','#','$','%','^','&','*',';',':','<','>','?','/'];
+
 // Write password to the #password input
-function writePassword() {
+//function writePassword() {
+
+  // X THEN I am presented with a series of prompts for password criteria - Done
+  var upperCasePrompt = confirm("Do you want uppercase characters in your password?");
+  //console.log(upperCasePrompt)
+  var lowerCasePrompt  = confirm("Do you want lowercase characters in your password?");
+  //console.log(lowerCasePrompt)
+  var numberPrompt  = confirm("Do you want numbers in your password?");
+  //console.log(numberPrompt)
+  var specialCharPrompt  = confirm("Do you want to include special characters in your password?");
+  //console.log(specialCharPrompt)
+
+//X THEN I select which criteria to include in the password
+
+//Create an empty password array
+  var passwordArr = []
+
+  // if (upperCasePrompt === true) || (lowerCasePrompt === true) || (numberPrompt === true) || (specialCharPrompt === true){
+  //   passwordArr.push(lowerCaseArr, upperCaseArr, numberArr, specialCharacterArr)
+  //   }
+  if (upperCasePrompt = true) {
+    passwordArr.push(upperCaseArr)
+    }
+  if (lowerCasePrompt = true) {
+    passwordArr.push(lowerCaseArr)
+  }
+  if (numberPrompt = true) {
+    passwordArr.push(numberArr)
+  }
+  if (specialCharPrompt = true) {
+    passwordArr.push(specialCharacterArr)
+  }
+//}
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
